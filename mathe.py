6c9fe833ff7a6d6917d95b3a28c358df01918e96
Ayoub code <104401493+AyoubAlmhedwe@users.noreply.github.com>
2023-02-14 14:06:28 -0800

    Revert "OOP"

--- mathe.py
+++ mathe.py
@@ -1,14 +1,2 @@
 u = ( 7/5 * 3) -2 + 1
 print(u)
-
-
-class Math():
-    def __init__(self):
-        self.fun()
-        self.func()
-    def fun(self):
-        self.value = 5
-        self.number = 10
-    def func(self):
-        print(self.value,self.number)
-Math()
